e5c9d4a8d13e079610fce2293049d0e233f9c3b4
GabrielBogo1 <gabrieldacruzbogo01@gmail.com>
2025-02-12 20:17:22 -0300

    product controller changes

--- src/main/java/com/listadecompras/listacompras/controller/productController.java
+++ src/main/java/com/listadecompras/listacompras/controller/productController.java
@@ -1,67 +1,64 @@
-// package com.listadecompras.listacompras.controller;
+package com.listadecompras.listacompras.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.dao.DataIntegrityViolationException;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.validation.annotation.Validated;
-// import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// import com.listadecompras.listacompras.entity.product;
-// import com.listadecompras.listacompras.repository.productRepository;
-// import com.listadecompras.listacompras.service.productService;
+import com.listadecompras.listacompras.entity.product;
+import com.listadecompras.listacompras.repository.productRepository;
+import com.listadecompras.listacompras.service.productService;
 
-// @Controller
-// @RequestMapping(value = "/api/product")
-// @CrossOrigin
-// public class productController {
+@Controller
+@RequestMapping(value = "api/product")
+@CrossOrigin
+public class productController {
     
-//     @Autowired
-//     productRepository productRepository;
+    @Autowired
+    productRepository productRepository;
 
-//     @Autowired
-//     productService productService;
+    @Autowired
+    productService productService;
 
 
-//     @GetMapping("/{id}")
-//     public ResponseEntity<product> findByIDPath(@PathVariable("id") final Long id) {
-//         final product product = this.productRepository.findById(id).orElse(null);
-//         return ResponseEntity.ok(product);
-//     }
+    @GetMapping("/{id}")
+    public ResponseEntity<product> findByIDPath(@PathVariable("id") final Long id) {
+        final product product = this.productRepository.findById(id).orElse(null);
+        return ResponseEntity.ok(product);
+    }
 
-//       @GetMapping
-//     public ResponseEntity<List <product>> listAll() {
-//         return ResponseEntity.ok(this.productRepository.findAll());
-//     }
+      @GetMapping
+    public ResponseEntity<List <product>> listAll() {
+        return ResponseEntity.ok(this.productRepository.findAll());
+    }
 
-//     @PostMapping
-//     public product createProduct (@RequestBody final product product) {
-//         try {
-//             return this.productService.validProduct(product);
-//         } catch (DataIntegrityViolationException e) {
-//             // String errorMessage = getErrorMessage(e);
-//             return null;
-//         }
-//     }
+    @PostMapping
+    public product createProduct (@RequestBody final product product) {
+        try {
+            return this.productService.save(product);
+        } catch (DataIntegrityViolationException e) {
+            return null;
+        }
+    }
 
-   
-//     @PutMapping ("/{id}")
-//     public ResponseEntity<HttpStatus> update(@PathVariable("id") final Long id, @RequestBody final product product) {
-//         try {
-//             productService.updateProduct(id,product);
-//             return ResponseEntity.ok(HttpStatus.CREATED);
-//         } catch (DataIntegrityViolationException e) {
-//             return new ResponseEntity<> (null, HttpStatus.BAD_REQUEST);
-//         }
-//     }
+    @PutMapping ("/{id}")
+    public ResponseEntity<HttpStatus> update(@PathVariable("id") final Long id, @RequestBody final product product) {
+        try {
+            productService.updateProduct(id,product);
+            return ResponseEntity.ok(HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<> (null, HttpStatus.BAD_REQUEST);
+        }
+    }
 
-// }
+}
--- src/main/java/com/listadecompras/listacompras/service/productService.java
+++ src/main/java/com/listadecompras/listacompras/service/productService.java
@@ -1,5 +1,31 @@
 package com.listadecompras.listacompras.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.listadecompras.listacompras.entity.product;
+import com.listadecompras.listacompras.repository.productRepository;
+
+@Service
 public class productService {
-    
+       
+    @Autowired
+    productRepository productRepository;
+
+    public product save(product product) {
+        return productRepository.save(product);
+    }
+
+    public void updateProduct (final Long id, final product product) {
+
+        final product productSaved = this.productRepository.findById(id).orElse(null);
+
+        if (productSaved == null || !productSaved.getId().equals(id)) {
+            throw new RuntimeException();
+        }
+
+        BeanUtils.copyProperties(product, productSaved);
+        this.productRepository.save(productSaved);
+    }
 }
