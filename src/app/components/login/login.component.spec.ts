fac1403973ecd10adf6eda1209b185c413c1ccdf
GabrielBogo1 <gabrieldacruzbogo01@gmail.com>
2025-03-09 15:22:42 -0300

    testandoodqwdwqdwqqdwqdwd

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,13 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../../auth/auth.service';
 import { ActivatedRoute } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
-
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -15,14 +12,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      declarations: [LoginComponent],
-    providers: [
-      { provide: ActivatedRoute, useValue: { params: of({}) } }, // Mock do ActivatedRoute
-      { provide: AuthService, useValue: { login: jasmine.createSpy('login') } } // Mock do AuthService
-    ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        LoginComponent
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({}) } }, 
+        { provide: AuthService, useValue: { login: jasmine.createSpy('login') } } 
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
