456eab05de6e9bf9974974ac28d22483c3e31716
GabrielBogo1 <gabrieldacruzbogo01@gmail.com>
2025-03-09 15:12:59 -0300

    testandoo

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,7 +11,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, HttpClientModule, AuthService]
+      imports: [LoginComponent, HttpClientModule, ActivatedRoute]
     })
     .compileComponents();
 
