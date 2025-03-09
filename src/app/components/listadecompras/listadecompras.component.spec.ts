d125698eb77b6e9e58b2317fe26f5a1ea62a6920
GabrielBogo1 <gabrieldacruzbogo01@gmail.com>
2025-03-09 15:10:07 -0300

    asdasdteasqdasdasw

--- src/app/components/listadecompras/listadecompras.component.spec.ts
+++ src/app/components/listadecompras/listadecompras.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListadecomprasComponent } from './listadecompras.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ListadecomprasComponent', () => {
   let component: ListadecomprasComponent;
@@ -9,7 +10,7 @@ describe('ListadecomprasComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListadecomprasComponent, HttpClientModule]
+      imports: [ListadecomprasComponent, HttpClientModule, ActivatedRoute]
     })
     .compileComponents();
 
--- src/app/editar-item/editar-item.component.spec.ts
+++ src/app/editar-item/editar-item.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditarItemComponent } from './editar-item.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('EditarItemComponent', () => {
   let component: EditarItemComponent;
@@ -9,7 +10,7 @@ describe('EditarItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditarItemComponent, HttpClientModule]
+      imports: [EditarItemComponent, HttpClientModule, ActivatedRoute]
     })
     .compileComponents();
 
