d9aa14cde7f13f7c8de03022a0c6a77e4f66e14a
GabrielBogo1 <gabrieldacruzbogo01@gmail.com>
2025-03-09 15:02:37 -0300

    teasqdw

--- src/app/services/produtos.service.spec.ts
+++ src/app/services/produtos.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProdutosService } from './produtos.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProdutosService', () => {
   let service: ProdutosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    providers: [ProdutosService]
+    });
     service = TestBed.inject(ProdutosService);
   });
 
